3516c76bc690681974685e4a443fdfb061fd825d
manitejabayyaa <maniteja.bayya369@gmail.com>
2025-01-07 17:51:57 +0530

    changed the into script file

--- Portfolio-Website-main/assets/js/script.js
+++ Portfolio-Website-main/assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "Machine Learning", "web development"],
+    strings: ["frontend development", "backend development", "web designing", "Machine Learning", "web development","Freelancer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
